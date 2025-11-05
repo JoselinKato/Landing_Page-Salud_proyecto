a012f790521649d89327479299e80d80b9fc6575
JoselinKato <kato2601@outlook.com>
2025-11-05 16:18:00 -0600

    Corrección de colores, añadi un navbar y conacto en footer, además de un logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,51 @@
 import Navbar from 'react-bootstrap/Navbar';
 import { Row, Col } from 'react-bootstrap';
 import '../styles/navbar.css'
+import Logo from "../../public/favicon.ico"
 
 function NavbarPrin() {
   return (
     <>
       <Navbar className="divPadre"  >
         <div >
-          <Navbar.Brand href="#home">
+          <Navbar.Brand >
             <Row>
-                <Col>
-                 {/* <img
+           <Col>
+            <img
               alt=""
-              src="/img/logo.svg"
-              width="30"
-              height="30"
+              src={Logo}
+              width="80"
+              height="80"
               className="d-inline-block align-top"
-            />{' '} */}
-                </Col>
-                <Col>
-                 <h2 style={{color:"white"}}>JuntosXSalud</h2>
+              style={{marginTop:"38%", marginLeft:"20%"}}
+            />{' '}
+             </Col>
+             <Col>
+
+                 <h2 style={{color:"white", marginTop:"22%"}}>SaludXChiapas</h2>
                 </Col>
             </Row>
           </Navbar.Brand>
         </div>
+        <div className="scroll-items">
+  <button onClick={() => document.getElementById("servicios").scrollIntoView({ behavior: "smooth" })}>
+    Servicios
+  </button>
+
+  <button onClick={() => document.getElementById("plataforma").scrollIntoView({ behavior: "smooth" })}>
+    Plataforma
+  </button>
+
+  <button onClick={() => document.getElementById("impacto").scrollIntoView({ behavior: "smooth" })}>
+    Impacto
+  </button>
+
+  <button onClick={() => document.getElementById("contacto").scrollIntoView({ behavior: "smooth" })}>
+    Contacto
+  </button>
+</div>
       </Navbar>
+      
     </>
   );
 }
